156e4fa5c5605315e2a6af58d64fc312da66c534
Azhar610 <azharaji.2022@student.uny.ac.id>
2025-09-21 23:36:57 +0700

    fixing berita dan artikel

--- berita-dan-artikel.php
+++ berita-dan-artikel.php
@@ -372,8 +372,8 @@ $artikel = [
 
         <nav class="navbar">
             <ul>
-                <li><a href="#">BERANDA</a></li>
-                <li><a href="#">STAFF</a></li>
+                <li><a href="index.php">BERANDA</a></li>
+                <li><a href="staff.php">STAFF</a></li>
                 <li><a href="#" class="active">BERITA & ARTIKEL</a></li>
             </ul>
         </nav>
