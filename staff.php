8865b7850ba120b7b583d19fddffe9a58277bbd8
Azhar610 <azharaji.2022@student.uny.ac.id>
2025-09-22 00:21:00 +0700

    staff nya ngikut dulu dah sama index

--- staff.php
+++ staff.php
@@ -179,7 +179,9 @@
 
     <div class="sticky-header-wrapper">
         <header class="header">
-            <div class="logo">Gambar</div>
+            <div class="logo">
+                <img src="Icon_Purworejo.png">
+            </div>
             <div class="title-info">
                 <h1>DESA SUMOWONO</h1>
                 <p>Kecamatan Kaligesing, Kabupaten Purworejo, Jawa Tengah</p>
